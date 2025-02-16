e24b158741ea601f89a127f511bf66fe850d5f9a
David Salathé <salathe.david@gmail.com>
2025-02-16 18:18:33 +0000

    Added background

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,14 +2,17 @@ import Header from './Header';
 
 function Layout({ children }) {
   return (
-    <div style={{
-      maxWidth: '800px',
-      margin: '0 auto',
-      padding: '20px'
-    }}>
-      <Header />
-      {children}
-    </div>
+    <>
+      <div className="mountain-background" />
+      <div style={{
+        maxWidth: '800px',
+        margin: '0 auto',
+        padding: '20px'
+      }}>
+        <Header />
+        {children}
+      </div>
+    </>
   );
 }
 
