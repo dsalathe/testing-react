25223db5071d7a3072d5891cb0b10d6c03ed5ff0
dsalathe <salathe.david@gmail.com>
2025-03-02 17:57:36 +0000

    Deploying to gh-pages from @ dsalathe/testing-react@4d6b59f886b5e7e3056e0d8cd23ac9312f621d21 🚀

---
+++ assets/002-entropy-f3e07e9c.js
@@ -0,0 +1,13 @@
+const n=`---
+id: 2
+title: What is the answer to the universe, the life and everything? Entropy.
+description: Coming soon
+publishedDate: 2025-02-16
+image: entropy.jpg
+keywords:
+  - coming soon
+---
+
+# What is the answer to the universe, the life and everything? Entropy.
+
+Coming soon`;export{n as default};
