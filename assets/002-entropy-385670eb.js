e38c617e4406dd01c86a1d7f73e2d5f5f315dd32
dsalathe <salathe.david@gmail.com>
2025-03-02 19:19:42 +0000

    Deploying to gh-pages from @ dsalathe/testing-react@603fbe8ba8d2ae2e2c00898ed11615c0a2039c19 🚀

--- assets/002-entropy-385670eb.js
+++ assets/002-entropy-385670eb.js
@@ -3,7 +3,7 @@ id: 2
 title: What is the answer to the universe, the life and everything? Entropy.
 description: Coming soon
 publishedDate: 2025-02-16
-image: entropy.jpg
+image: entropy.webp
 keywords:
   - coming soon
 ---
---
+++ assets/004-fourth-post-dac7fc88.js
@@ -0,0 +1,13 @@
+const n=`---
+id: 4
+title: Will AI be smarter than humans?
+description: Coming soon
+publishedDate: 2025-02-15
+image: aivshuman.webp
+keywords:
+  - coming soon
+---
+
+# Will AI be smarter than humans?
+
+Coming Soon`;export{n as default};
